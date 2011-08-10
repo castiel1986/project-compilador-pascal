d260b1e236757cf57ef1e40f8ea7f0a3eeeb3908
evertonjsilva31@gmail.com <evertonjsilva31@gmail.com@01bd7453-b1f0-5ca1-259d-ab9d81987c5a>
2011-08-10 03:38:15 +0000

    Declaracao e chamada de funcao ok.

---
+++ trunk/src/Funcao.java
@@ -0,0 +1,59 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Everton
+ */
+public class Funcao implements InfoComplementar, ElemPilhaSem{
+    
+    private int rotulo;
+    private Tipo tipo;
+    private int deslocamento;
+    private ArrayList<Parametro> parametros;
+
+    public Funcao(int rotulo, Tipo tipo, int deslocamento) {
+        this.rotulo = rotulo;
+        this.tipo = tipo;
+        this.deslocamento = deslocamento;
+        this.parametros = new ArrayList<Parametro>();
+    }
+
+    public int getDeslocamento() {
+        return deslocamento;
+    }
+
+    public void setDeslocamento(int deslocamento) {
+        this.deslocamento = deslocamento;
+    }
+    
+    public int getRotulo() {
+        return rotulo;
+    }
+
+    public void setRotulo(int rotulo) {
+        this.rotulo = rotulo;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void addParametro(Parametro parametro) {
+        this.parametros.add(parametro);
+    }
+    
+    public Parametro getParametro(int index){
+        return parametros.get(index);
+    }
+    
+}
